f51a7a341a693d37039c0ebedd7360d3800104cb
aptt <git@aptt.ch>
2023-07-26 18:51:10 +0200

    refactor(app): 💡 extract settings modal to its own component
    and trigger it via event from header

---
+++ packages/app/src/components/settings_modal.svelte
@@ -0,0 +1,87 @@
+<script>
+  import { character_set } from '$stores/character_set.mjs'
+  import { show_pinyin } from '$stores/show_pinyin.mjs'
+  import { show_jyutping } from '$stores/show_jyutping.mjs'
+
+  import Modal from './modal.svelte'
+
+  /** @type {boolean} */
+  export let open
+</script>
+
+<Modal
+  heading="Site-wide settings"
+  bind:visible={open}
+>
+  <form on:submit|preventDefault>
+    <fieldset>
+      <legend>Chinese characters</legend>
+
+      <div
+        role="presentation"
+        class="input_group"
+      >
+        <label>
+          <input
+            type="radio"
+            name="character_set"
+            value="traditional"
+            bind:group={$character_set}
+          />
+          <span>Show traditional</span>
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="character_set"
+            value="simplified"
+            bind:group={$character_set}
+          />
+          <span>Show simplified</span>
+        </label>
+      </div>
+    </fieldset>
+
+    <fieldset>
+      <legend>Romanization</legend>
+
+      <label>
+        <input
+          type="checkbox"
+          bind:checked={$show_pinyin}
+        />
+        <span>Show pinyin</span>
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          bind:checked={$show_jyutping}
+        />
+        <span>Show jyutping</span>
+      </label>
+    </fieldset>
+  </form>
+</Modal>
+
+<style>
+  form {
+    display: grid;
+    gap: 2rem;
+    margin-top: 2rem;
+  }
+
+  legend {
+    margin-bottom: 0.25rem;
+    font-weight: 500;
+  }
+
+  label {
+    display: block;
+  }
+
+  .input_group {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+  }
+</style>
--- packages/app/src/routes/(app)/+layout.svelte
+++ packages/app/src/routes/(app)/+layout.svelte
@@ -1,11 +1,21 @@
 <script>
   import Footer from '$components/footer.svelte'
   import Header from '$components/header.svelte'
+  import SettingsModal from '$components/settings_modal.svelte'
 
   import { main_inert } from '$stores/main_inert.mjs'
+
+  /** @type {boolean} */
+  let settings_open = false
 </script>
 
-<Header />
+<Header
+  on:toggle_settings={() => {
+    settings_open = !settings_open
+  }}
+/>
+
+<SettingsModal open={settings_open} />
 
 <main inert={$main_inert}>
   <slot />
