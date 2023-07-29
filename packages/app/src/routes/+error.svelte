9904c6fe88d87b286d30eda4bb4bbf5c8bc1b668
aptt <git@aptt.ch>
2023-07-29 15:49:32 +0200

    feat(app): 🎸 add a basic error page

---
+++ packages/app/src/routes/+error.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { page } from '$app/stores'
+  import Head from '$components/head.svelte'
+
+  $: heading = $page.status === 404 ? 'This page does not exist' : "Oh no, something's broken"
+</script>
+
+<Head
+  title={heading}
+  description="An error occurred."
+/>
+
+<div>
+  <h1>{heading}</h1>
+
+  <p>
+    Try visiting
+    <a
+      href="/"
+      class="cd_link"
+    >
+      the search page
+    </a>
+    or
+    <a
+      href="/dictionary"
+      class="cd_link"
+    >
+      the dictionary
+    </a>
+    .
+  </p>
+</div>
+
+<style>
+  h1 {
+    font-weight: 500;
+    font-size: 1.25rem;
+  }
+
+  p {
+    margin-top: 1rem;
+  }
+
+  div {
+    display: grid;
+    align-content: center;
+    margin-inline: var(--margin_content_text);
+    height: 100vh;
+  }
+</style>
