b6d021a8125690617d91d731bf89c567fba63a30
aptt <git@aptt.ch>
2023-12-08 16:02:52 +0100

    refactor(app): 💡 remove unused store

--- packages/app/src/stores/revise_direction.mjs
+++
@@ -1,7 +0,0 @@
-import { local_storage_store } from './local_storage_store.mjs'
-
-/** @type {import('svelte/store').Writable<'chinese_to_native' | 'native_to_chinese'>} */
-export const revise_direction = local_storage_store({
-  key: 'settings/revise/direction',
-  value: 'chinese_to_native',
-})
