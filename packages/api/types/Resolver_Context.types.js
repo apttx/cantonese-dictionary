645243134bbe251e5f4ebd4d6eecbf6d9acd9c86
aptt <git@aptt.ch>
2023-12-14 14:18:21 +0100

    feat(api): 🎸 add single phrase query

--- packages/api/types/Resolver_Context.types.js
+++ packages/api/types/Resolver_Context.types.js
@@ -2,6 +2,7 @@
  * @typedef {{
  *   search: (options: { term: string; limit: number }) => Promise<Phrase[]>
  *   many: (options: { limit: number }) => Promise<Phrase[]>
+ *   one: (options: { id: string }) => Promise<Phrase>
  * }} Phrases_Datasource
  */
 
