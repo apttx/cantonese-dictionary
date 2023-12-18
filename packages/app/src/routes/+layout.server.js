4002033ffd2e13561a57033cfbf6762755470911
aptt <git@aptt.ch>
2023-12-18 15:27:42 +0100

    feat(app): 🎸 enable infinite isr w/ bypass token

---
+++ packages/app/src/routes/+layout.server.js
@@ -0,0 +1,12 @@
+// @ts-expect-error broken env types
+import { ISR_BYPASS_TOKEN } from '$env/static/private'
+
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+  // incremental static regeneration
+  isr: {
+    // never expire
+    expiration: false,
+    bypassToken: ISR_BYPASS_TOKEN,
+  },
+}
