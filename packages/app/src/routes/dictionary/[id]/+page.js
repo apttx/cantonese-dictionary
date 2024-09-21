cb590bcfef9d2d64f848e5aca0d600b154318c9c
aptt <git@aptt.ch>
2024-09-21 11:03:11 +0200

    feat(app): 🎸 only isr phrase detail pages instead of everything

--- packages/app/src/routes/dictionary/[id]/+page.js
+++ packages/app/src/routes/dictionary/[id]/+page.js
@@ -1,6 +1,17 @@
 import { error } from '@sveltejs/kit'
-
 import { client, gql } from '$graphql'
+// @ts-expect-error broken env types
+import { ISR_BYPASS_TOKEN } from '$env/static/private'
+
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+  // incremental static regeneration
+  isr: {
+    // never expire
+    expiration: false,
+    bypassToken: ISR_BYPASS_TOKEN,
+  },
+}
 
 /** @type {import('@urql/core').TypedDocumentNode<{ phrase: Phrase }, { id: string }>} */
 const phrase_query = gql`
