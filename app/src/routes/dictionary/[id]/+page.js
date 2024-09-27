d004f67a136461f612cd2e3ccae5c02f50043a6d
aptt <git@aptt.ch>
2024-09-27 18:27:45 +0200

    refactor(app): 💡 remove unnecessary isr bypass token

--- app/src/routes/dictionary/[id]/+page.js
+++ app/src/routes/dictionary/[id]/+page.js
@@ -1,7 +1,5 @@
 import { error } from '@sveltejs/kit'
 import { client, gql } from '$graphql'
-// @ts-expect-error broken env types
-import { ISR_BYPASS_TOKEN } from '$env/static/private'
 
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 export const config = {
@@ -9,7 +7,6 @@ export const config = {
   isr: {
     // never expire
     expiration: false,
-    bypassToken: ISR_BYPASS_TOKEN,
   },
 }
 
