aac4a5ffcf7307eb686c972b8523647a72de0df0
aptt <git@aptt.ch>
2024-12-31 13:35:33 +0100

    feat(app): 🎸 always build for vercel

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,8 +1,5 @@
-import auto from '@sveltejs/adapter-auto'
 import vercel from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-const adapter = process.env.USE_VERCEL_ADAPTER ? vercel({ runtime: 'nodejs20.x' }) : auto()
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +8,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter,
+    adapter: vercel(),
     alias: {
       $components: 'src/components/*',
       $stores: 'src/stores/*',
