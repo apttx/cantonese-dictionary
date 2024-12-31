84e52ed0e128cc4c741b4f6020fbc6a3660c7fa3
aptt <git@aptt.ch>
2024-12-31 14:46:24 +0100

    fix(app): 🐛 move character set type to types folder

--- app/src/stores/character_set.ts
+++ app/src/stores/character_set.ts
@@ -1,5 +1,5 @@
 import { local_storage_store } from './local_storage_store.ts'
-import { type Character_Set } from '../../types/Character_Set.ts'
+import { type Character_Set } from '$types/Character_Set.ts'
 
 export const character_set = local_storage_store<Character_Set>({
   key: 'settings/characters',
