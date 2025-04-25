cbea49e55d00c62d3e049deadd4e9af0e6dc343e
aptt <git@aptt.ch>
2025-04-25 20:48:00 +0200

    feat(app): 🎸 add canonical url to pages

--- app/src/components/head.svelte
+++ app/src/components/head.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
-  export let title: string
-  export let description: string
+  import { page } from '$app/state'
+  import { PUBLIC_CANONICAL_BASE_URL } from '$env/static/public'
+
+  let {
+    title,
+    description,
+  }: {
+    title: string
+    description: string
+  } = $props()
+
+  const canonicalUrl = $derived.by(() => {
+    const canonicalUrl = new URL(PUBLIC_CANONICAL_BASE_URL)
+
+    canonicalUrl.pathname = page.url.pathname
+    canonicalUrl.search = page.url.search
+    canonicalUrl.hash = page.url.hash
+
+    return canonicalUrl
+  })
 </script>
 
 <svelte:head>
@@ -8,5 +26,9 @@
   <meta
     name="description"
     content={description}
+  />
+  <link
+    rel="canonical"
+    href={canonicalUrl.toString()}
   />
 </svelte:head>
